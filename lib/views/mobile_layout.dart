5686a500c20696bf6b7f0aaeaceaa6d398134232
H0SAM7 <hoadel2003@gmail.com>
2024-08-22 01:36:22 +0300

    finsh monile layout

--- lib/views/mobile_layout.dart
+++ lib/views/mobile_layout.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/all_expenses.dart';
+import 'package:responsive_dashboard/widgets/drawer.dart';
 import 'package:responsive_dashboard/widgets/income.dart';
 import 'package:responsive_dashboard/widgets/my_card_section.dart';
 import 'package:responsive_dashboard/widgets/quick_invoice.dart';
 import 'package:responsive_dashboard/widgets/transction_history.dart';
 
-class MobileLayoutView extends StatelessWidget {
+class MobileLayoutView extends StatefulWidget {
   const MobileLayoutView({super.key});
 
   @override
+  State<MobileLayoutView> createState() => _MobileLayoutViewState();
+}
+
+class _MobileLayoutViewState extends State<MobileLayoutView> {
+  final GlobalKey<ScaffoldState> scaffoldkey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: MediaQuery.sizeOf(context).width <800?AppBar(
-        elevation: 0,
-        leading: const Icon(Icons.menu,color: Color(0xFF4EB7F2),),
-      ):null,
+    return Scaffold(
+      key: scaffoldkey,
+      appBar: MediaQuery.sizeOf(context).width < 800
+          ? AppBar(
+              elevation: 0,
+              leading: IconButton(
+                icon: const Icon(Icons.menu, color: Color(0xFF4EB7F2)),
+                onPressed: () {
+                 scaffoldkey.currentState!.openDrawer();
+                },
+              ),
+            )
+          : null,
       body: const SingleChildScrollView(
         child: Column(
           children: [
-         
             AllExpenses(),
             SizedBox(
               height: 24,
@@ -30,14 +44,8 @@ class MobileLayoutView extends StatelessWidget {
           ],
         ),
       ),
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
     );
   }
 }
-
-
-
-
-
-
-
-
